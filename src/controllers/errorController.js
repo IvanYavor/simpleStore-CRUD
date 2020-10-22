3da5ef9709da9088daf052543c6e164c5aad4f11
IvanYavor <02ivan09@gmail.com>
2020-10-22 18:45:45 +0300

    update error controller

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,6 +1,23 @@
 const AppError = require('../helpers/appError');
 
-const handleCastErrorDb = (err) => {
+const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path} : ${err.value}.`;
   return new AppError(message, 400);
+};
+
+const sendError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (err.name === 'CastError') err = handleCastErrorDB(err);
+  sendError(err, res);
 };
